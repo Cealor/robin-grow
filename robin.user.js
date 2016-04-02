1abd0d2c0cae8ec0e73b7168905c746030706f10
Cealor <cealorify@gmail.com>
2016-04-02 17:53:14 +0200

    Update robin.user.js

--- robin.user.js
+++ robin.user.js
@@ -207,6 +207,64 @@
         }
     }
 
+
+
+    //Spam filter by gardner
+    var percentNonWordCharactersAllowed = .25;
+    var ratioUppercaseToLowercase = .25;
+
+    var filters = [ 'Robin Autovoter', 'nigger', 'Faggot', 'faggot', 'com.crackedglass.polydodge.droid', 'Autovote' ];
+
+    var msgsDeleted = 0;
+
+    // This is the function that checks for ALL CAPS MESSAGES and poop emojis
+    function isReal(s) {
+    var upperStr = s.replace(/[A-Z]/g, '')
+    var upper = s.length - upperStr.length;
+  
+    var lowerStr = upperStr.replace(/[a-z]/g, '')
+    var lower = upperStr.length - lowerStr.length;
+  
+    var nonWord = lowerStr.length;
+
+    // if the ratio of uppercase to lowercase letters is greater that .25, fail the message
+    if ((upper / lower) > ratioUppercaseToLowercase) {
+    //    console.log(upper + ' / ' + lower + ' / ' + nonWord + ' ratio is ' + upper / lower);
+    return false;
+  }
+
+    // if the percentage of non-word characters is larger than 25% then fail the message
+    if ((nonWord / s.length) > percentNonWordCharactersAllowed) {
+    //    console.log(upper + ' / ' + lower + ' / ' + nonWord + ' percentage is ' + nonWord / s.length);
+    return false;
+  }
+  
+  for (var i = 0; i < filters.length; i++) {
+    if (s.indexOf(filters[i]) >=0 ) {
+      // the string contains one of the filter words
+      
+      return false
+    }
+  }
+  
+  return true
+}
+
+
+$("#robinChatMessageList").bind("DOMSubtreeModified", function() {
+  var msgs = $('.robin-message--message');
+//  console.log('number of msgs: ' + msgs.length + '/' + msgsDeleted);
+
+  for(var i = 0; i < msgs.length; i++) {
+    if (!isReal(msgs[i].innerText)) {
+      // allow the system messages to come through
+      if (!$(msgs[i]).parent().hasClass('robin--user-class--system')) {
+        msgsDeleted++;
+        $(msgs[i]).parent().remove();
+      }
+    }
+  }
+});
     // credit to wwwroth for idea
     // i think this method is better
 
