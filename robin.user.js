0af1de58893adbd9e8684796487f4f401315e129
Cealor <cealorify@gmail.com>
2016-04-02 20:22:02 +0200

    Update robin.user.js

--- robin.user.js
+++ robin.user.js
@@ -27,13 +27,15 @@ var manualThaiList = ["̍", "̎", "̄", "̅", "̿", "̑", "̆", "̐", "͒", "͗"
 "];
 
 var spamBlacklist = [
+  "spam the most used",
   "ຈل͜ຈ", "hail the", "autovoter", "staying", "﷽", "group to stay", "pasta",
   "automatically voted", "stayers are betrayers", "stayers aint players",
   "mins remaining. status", ">>>>>>>>>>>>>>>>>>>>>>>",
   "TRUMPSBUTTPIRATES2016", "TRUMPSFIERYPOOPS2016",
   "ALL HAIL THE TACO BELL BOT", "#420", "้", "็", "◕_◕",
   "<<<<<<<<<<<<<<<<<<<<<<", "growing is all we know", "f it ends on you",
-  "timecube", "\( ͡° ͜ʖ ͡°\)", "◕", "guys can you please not spam the chat"
+  "timecube", "\( ͡° ͜ʖ ͡°\)", "◕", "guys can you please not spam the chat", "ｍｅｍｅｓ ｏｆ ｃａｐｉｔａｌｉｓｍ",
+  "𝐁𝐄𝐑𝐍𝐈𝐄 𝐒𝐀𝐍𝐃𝐌𝐀𝐍", "█▄█▄"
 ];
 
 var nonEnglishSpamRegex = "[^\x00-\x7F]+";
